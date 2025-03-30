df484e953ce54ef7f142abd3b52d59143970f430
hOoH1125 <2242296174@qq.com>
2025-03-30 23:03:22 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,1 @@
 #include<iostream>
-#include<iomanip>
-#include"Matrix.h"
-using namespace std;
-int main()
-{
-	int i;
-	Matrix M1(1, 2, 3, 4), M2, M3(4, 3, 2, 1);
-	cout << "M1: " << M1;
-	cout << endl;
-	cout << "请输入M2中的四个数字: ";
-	cin >> M2;
-	cout << "M1+M2: " << M1 + M2;
-	cout << "M1-M2: " << M1 - M2;
-	cout << "M1*M2: " << M1 * M2;
-	cout << "M3: " << M3;
-	cout << "M1+M2+M3: " << M1 + M2 + M3;
-	cout << "M1*M2*M3: " << M1 * M2 * M3;
-}
